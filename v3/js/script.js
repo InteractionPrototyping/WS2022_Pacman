5315c4e79373bfeafd9b37829e102b2912297d4c
juliamgdln <julia.kadur@tum.de>
2021-12-12 15:15:00 +0100

    update julia

--- v3/js/script.js
+++ v3/js/script.js
@@ -51,6 +51,64 @@ $(document).ready(function() {
     });
 });
 
+$(document).ready(function() {
+    $('#save-get-suggestions').on('click', function() {
+        window.location.href = 'you_are_set.html';
+    });
+});
+
+$(document).ready(function() {
+    $('#close-ingredients').on('click', function() {
+        window.location.href = 'select_pantry.html';
+    });
+});
+
+$(document).ready(function() {
+    $('#open-ingredients').on('click', function() {
+        window.location.href = 'select_pantry_selected.html';
+    });
+});
+
+$(document).ready(function() {
+    $('#select-ingr-later').on('click', function() {
+        window.location.href = 'you_are_set.html';
+    });
+});
+
+
+
+/* ------------- 
+
+##################################
+####### PANTRY STORAGE ###########
+################################## 
+
+---------------*/
+
+$(document).ready(function() {
+    $('#open-ingredient-options').on('click', function() {
+        window.location.href = 'pantry2.html';
+    });
+});
+
+$(document).ready(function() {
+    $('#close-ingredient-options').on('click', function() {
+        window.location.href = 'pantry.html';
+    });
+});
+
+$(document).ready(function() {
+    $('#save-update-recipes').on('click', function() {
+        window.location.href = 'update_recipes.html';
+    });
+});
+
+$(document).ready(function() {
+    $('#save-update-recipes2').on('click', function() {
+        window.location.href = 'update_recipes.html';
+    });
+});
+
 /* ------------- 
 
 ##################################
@@ -69,3 +127,32 @@ $(document).ready(function() {
 ################################## 
 
 ---------------*/
+
+
+/* ------------- 
+
+##################################
+########## COOKBOOK ##############
+################################## 
+
+---------------*/
+
+$(document).ready(function() {
+    $('#open-recipe').on('click', function() {
+        window.location.href = 'recipe_ingredients.html';
+    });
+});
+
+/* ------------- 
+
+##################################
+########### PROFILE ##############
+################################## 
+
+---------------*/
+
+$(document).ready(function() {
+    $('#open-pantry-storage').on('click', function() {
+        window.location.href = 'pantry.html';
+    });
+});
