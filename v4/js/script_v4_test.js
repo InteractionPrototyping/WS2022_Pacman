816b8b380a21dc9c1bb3b94e91eca3eeed31e03a
Christine Dichtl <cdichtl.fs@gmail.com>
2021-12-22 19:25:23 +0100

    test

--- v4/js/script_v4_test.js
+++ v4/js/script_v4_test.js
@@ -7,12 +7,14 @@
 ---------------*/
 
 $(document).ready(function() {
+    $("#select-diet").hide();
 
     $('#lets-start').on('click', function() {
         $(".title").animate({ top: -80 }, 2000);
         $(".text-1").animate({ top: -30 }, 2100);
         $(".overlap-group").animate({ top: -500 }, 1500, function() {
-            // window.location.href = 'select_diet.html';
+            $("#welcome").hide();
+            $("#select-diet").show();
         });
     });
 });
