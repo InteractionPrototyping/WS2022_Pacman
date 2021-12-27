86065ae0a6cbcf4b571377fc4e4d77fcc3e77fae
Christine Dichtl <cdichtl.fs@gmail.com>
2021-12-27 21:19:01 +0100

    update

--- v4/js/script_v4_test.js
+++ v4/js/script_v4_test.js
@@ -11,10 +11,14 @@ $(document).ready(function() {
 
     $('#lets-start').on('click', function() {
         $(".title").animate({ top: -80 }, 2000);
-        $(".text-1").animate({ top: -30 }, 2100);
+        $("#welcome .text-1").animate({ top: -30 }, 2100);
         $(".overlap-group").animate({ top: -500 }, 1500, function() {
             $("#welcome").hide();
             $("#select-diet").show();
         });
     });
+
+    // $('#select-diet .vegeterian-option').on('click', function() {
+
+    // });
 });
