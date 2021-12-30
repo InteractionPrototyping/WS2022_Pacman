2d077364155984254130f27a3adcf3a5e58db661
Christine Dichtl <cdichtl.fs@gmail.com>
2021-12-30 12:46:31 +0100

    update

--- v4/js/script_v4.js
+++ v4/js/script_v4.js
@@ -1,17 +1,44 @@
-/* ------------- 
-
-##################################
-########## WELCOME  ##############
-################################## 
-
----------------*/
-
 $(document).ready(function() {
+    $("#select-diet").hide();
+    $("#select-pantry").hide();
+    /* ------------- 
+
+    ##################################
+    ########## WELCOME  ##############
+    ################################## 
+
+    ---------------*/
+
+    /* --- animation after clicking on lets's start --- */
     $('#lets-start').on('click', function() {
-        $(".title").animate({ top: -200 }, 2000);
-        $(".text-1").animate({ top: -100 }, 2000);
-        $(".welcome-background-style").animate({ top: -5000 }, 1500, function() {
-            // window.location.href = 'select_diet.html';
+        $(".title").animate({ top: -80 }, 2000);
+        $("#welcome .text-1").animate({ top: -30 }, 2100);
+        $(".overlap-group").animate({ top: -500 }, 1500);
+        $("#select-diet").fadeIn(2500, function() {
+            $("#welcome").hide();
         });
     });
+
+    /* ------------- 
+
+    ##################################
+    ########## SELECT DIET ###########
+    ################################## 
+
+    ---------------*/
+
+    $('#save-diet-settings').on('click', function() {
+        $("#select-pantry").fadeIn(2000);
+        $("#select-diet").fadeOut(2000);
+    });
+    $('#no-special-diet').on('click', function() {
+        $("#select-pantry").fadeIn(2000);
+        $("#select-diet").fadeOut(2000);
+    });
+
+
+    /* --- choose your diet / change color of button after click --- */
+    // $('#select-diet .vegeterian-option').on('click', function() {
+    //             $(this).css("background-color", "blue"
+    //             });
 });
