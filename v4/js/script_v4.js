4d6c98190cca2eba8ad8a29f09234365cc080e37
Christine Dichtl <cdichtl.fs@gmail.com>
2021-12-30 14:33:40 +0100

    update

--- v4/js/script_v4.js
+++ v4/js/script_v4.js
@@ -1,44 +1,86 @@
 $(document).ready(function() {
+    $("#welcome").hide();
     $("#select-diet").hide();
     $("#select-pantry").hide();
-    /* ------------- 
+    $("#pantry-selection-cereals").hide();
+    $("#main").hide();
+    $("#cookbook").hide();
+    $("#pantry-bar").hide();
+    $("#pantry-bar-cereals").hide();
+    $("#user-profile").show();
+    // /* ------------- 
 
-    ##################################
-    ########## WELCOME  ##############
-    ################################## 
+    // ##################################
+    // ########## WELCOME  ##############
+    // ################################## 
 
-    ---------------*/
+    // ---------------*/
 
-    /* --- animation after clicking on lets's start --- */
-    $('#lets-start').on('click', function() {
-        $(".title").animate({ top: -80 }, 2000);
-        $("#welcome .text-1").animate({ top: -30 }, 2100);
-        $(".overlap-group").animate({ top: -500 }, 1500);
-        $("#select-diet").fadeIn(2500, function() {
-            $("#welcome").hide();
-        });
-    });
+    // /* --- animation after clicking on lets's start --- */
+    // $('#lets-start').on('click', function() {
+    //     $(".title").animate({ top: -80 }, 2000);
+    //     $("#welcome .text-1").animate({ top: -30 }, 2100);
+    //     $(".overlap-group").animate({ top: -500 }, 1500);
+    //     $("#select-diet").fadeIn(2500, function() {
+    //         $("#welcome").hide();
+    //     });
+    // });
 
-    /* ------------- 
+    // /* ------------- 
 
-    ##################################
-    ########## SELECT DIET ###########
-    ################################## 
+    // ##################################
+    // ########## SELECT DIET ###########
+    // ################################## 
 
-    ---------------*/
+    // ---------------*/
 
-    $('#save-diet-settings').on('click', function() {
-        $("#select-pantry").fadeIn(2000);
-        $("#select-diet").fadeOut(2000);
-    });
-    $('#no-special-diet').on('click', function() {
-        $("#select-pantry").fadeIn(2000);
-        $("#select-diet").fadeOut(2000);
-    });
+    // $('#save-diet-settings').on('click', function() {
+    //     $("#select-pantry").fadeIn(2000);
+    //     $("#select-diet").fadeOut(2000);
+    // });
+    // $('#no-special-diet').on('click', function() {
+    //     $("#select-pantry").fadeIn(2000);
+    //     $("#select-diet").fadeOut(2000);
+    // });
 
 
-    /* --- choose your diet / change color of button after click --- */
-    // $('#select-diet .vegeterian-option').on('click', function() {
-    //             $(this).css("background-color", "blue"
-    //             });
+    // /* --- choose your diet / change color of button after click --- */
+    // // $('#select-diet .vegeterian-option').on('click', function() {
+    // //             $(this).css("background-color", "blue"
+    // //             });
+
+
+    // /* ------------- 
+
+    // ##################################
+    // ####### SELECT PANTRY ############
+    // ################################## 
+
+    // ---------------*/
+
+    // $('#cereals').on('click', function() {
+    //     $("#pantry-selection-cereals").show();
+    //     $("#select-pantry").hide();
+    // });
+
+
+    // $('#save-get-recipe-suggestions').on('click', function() {
+    //     $("#main").fadeIn();
+    //     $("#select-pantry").fadeOut();
+    // });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
