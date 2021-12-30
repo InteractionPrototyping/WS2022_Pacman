7721026ba87c5d3b79a7a3164a9d1db08938f93a
Christine Dichtl <cdichtl.fs@gmail.com>
2021-12-30 15:22:49 +0100

    update

--- v4/js/script_v4.js
+++ v4/js/script_v4.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $("#welcome").hide();
+    $("#welcome").show();
     $("#select-diet").hide();
     $("#select-pantry").hide();
     $("#pantry-selection-cereals").hide();
@@ -12,70 +12,79 @@ $(document).ready(function() {
     $("#recipe-ingredients").hide();
     $("#recipe-video").hide();
     $("#you-are-set").hide();
-    $("#updating-recipes").show();
-    // /* ------------- 
+    $("#updating-recipes").hide();
+    /* ------------- 
 
-    // ##################################
-    // ########## WELCOME  ##############
-    // ################################## 
+    ##################################
+    ########## WELCOME  ##############
+    ################################## 
 
-    // ---------------*/
+    ---------------*/
 
-    // /* --- animation after clicking on lets's start --- */
-    // $('#lets-start').on('click', function() {
-    //     $(".title").animate({ top: -80 }, 2000);
-    //     $("#welcome .text-1").animate({ top: -30 }, 2100);
-    //     $(".overlap-group").animate({ top: -500 }, 1500);
-    //     $("#select-diet").fadeIn(2500, function() {
-    //         $("#welcome").hide();
-    //     });
-    // });
+    /* --- animation after clicking on lets's start --- */
+    $('#lets-start').on('click', function() {
+        $(".title").animate({ top: -80 }, 2000);
+        $("#welcome .text-1").animate({ top: -30 }, 2100);
+        $(".overlap-group").animate({ top: -500 }, 1500);
+        $("#select-diet").fadeIn(2500, function() {
+            $("#welcome").hide();
+        });
+    });
 
-    // /* ------------- 
+    /* ------------- 
 
-    // ##################################
-    // ########## SELECT DIET ###########
-    // ################################## 
+    ##################################
+    ########## SELECT DIET ###########
+    ################################## 
 
-    // ---------------*/
+    ---------------*/
 
-    // $('#save-diet-settings').on('click', function() {
-    //     $("#select-pantry").fadeIn(2000);
-    //     $("#select-diet").fadeOut(2000);
-    // });
-    // $('#no-special-diet').on('click', function() {
-    //     $("#select-pantry").fadeIn(2000);
-    //     $("#select-diet").fadeOut(2000);
-    // });
-
-
-    // /* --- choose your diet / change color of button after click --- */
-    // // $('#select-diet .vegeterian-option').on('click', function() {
-    // //             $(this).css("background-color", "blue"
-    // //             });
+    $('#save-diet-settings').on('click', function() {
+        $("#select-pantry").fadeIn(2000);
+        $("#select-diet").fadeOut(2000);
+    });
+    $('#no-special-diet').on('click', function() {
+        $("#select-pantry").fadeIn(2000);
+        $("#select-diet").fadeOut(2000);
+    });
 
 
-    // /* ------------- 
-
-    // ##################################
-    // ####### SELECT PANTRY ############
-    // ################################## 
-
-    // ---------------*/
-
-    // $('#cereals').on('click', function() {
-    //     $("#pantry-selection-cereals").show();
-    //     $("#select-pantry").hide();
-    // });
+    /* --- choose your diet / change color of button after click --- */
+    // $('#select-diet .vegeterian-option').on('click', function() {
+    //             $(this).css("background-color", "blue"
+    //             });
 
 
-    // $('#save-get-recipe-suggestions').on('click', function() {
-    //     $("#main").fadeIn();
-    //     $("#select-pantry").fadeOut();
-    // });
+    /* ------------- 
+
+    ##################################
+    ####### SELECT PANTRY ############
+    ################################## 
+
+    ---------------*/
+
+    $('#cereals').on('click', function() {
+        $("#pantry-selection-cereals").show();
+        $("#select-pantry").hide();
+    });
 
 
+    $('#save-get-recipe-suggestions').on('click', function() {
+        $("#main").fadeIn();
+        $("#select-pantry").fadeOut();
+    });
+    /* ------------- 
 
+    ##################################
+    ############# MAIN ###############
+    ################################## 
+
+    ---------------*/
+
+    $('#see-recipe').on('click', function() {
+        $("#recipe-ingredients").show("slide", { direction: "left" }, 1000);
+        $('#main').hide();
+    });
 
 
 
